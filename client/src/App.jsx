54c23664fb7ef85dcb612207f1f07dc2c46f4661
kyros-2 <zakariawidady74@gmail.com>
2024-04-09 15:02:18 +0000

    ded

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,6 @@
-import { useEffect } from "react";
-
 function App() {
   const endPoint = import.meta.env.VITE_END_POINT
 
-  useEffect(() => {
-    console.log(endPoint);
-  }, [])
   return (
     <h1>
       <button
--- server/index.js
+++ server/index.js
@@ -1,38 +1,34 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require('cors')
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-    origin: [process.env.ORIGIN]
-}));
+    origin: [process.env.ORIGIN],
+  })
+);
 
-
-const cookieDays = (days) => {
-  return days * 1000 * 60 * 60 * 24;
-};
 
 app.get("/", (req, res) => {
-  res.send("ttt");
+  res.send(process.env.ORIGIN);
 });
 
-// cookies :
 app.get("/set-cookie", (req, res) => {
-  res.cookie('zaka', "ria").send()
+  res.cookie("name", "value").send("Done");
 });
 
 app.get("/get-cookie", async (req, res) => {
-  // const { acces_token } = req.cookies;
-  res.json(req.cookies || {});
+  res.json(req.cookies);
 });
 
 app.get("/delete-cookie", (req, res) => {
-  res.clearCookie("zaka").send("Deleted !!");
+  res.clearCookie("name").send("Done");
 });
 
 const PORT = 8000;
